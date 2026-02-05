2988ba355b3428c30d2a09fa456e7ca79b479842
k5nnedy <ke83062p@pace.edu>
2026-02-04 19:10:22 -0500

    test server complete

--- TestClient.java
+++ TestClient.java
@@ -6,8 +6,27 @@ public class TestClient {
         try{
 
             System.out.println("Client started");
-            //creating socket on localhost because client and server are using the same machine
+            // creating socket on localhost because client and server are using the same machine
             Socket socket = new Socket("localhost", 9806);
+            // Creating a buffered reader an entire string.
+            // use input streamreader to read the byte stream (system.in) to give us a character stream
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+            // Output stream, true is used to auto-flush the data being sent
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.println("Enter a string");
+            // using buffered reader object to read a string from the keyboard
+            // readline waits for user to enter a string, that string is set to str
+            String str = userInput.readLine();
+
+            // Sending the data
+            out.println(str);
+
+            // Second buffered reader to read data the server sends
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // Printing the data from the server
+            System.out.println(in.readLine());
 
         } catch(Exception e) {
             e.printStackTrace();
